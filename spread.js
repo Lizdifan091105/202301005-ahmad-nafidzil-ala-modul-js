b32fd18a4b4a20c43c188d9331ef32bd32bc97c4
ahmad nafidzil ala <162394204+Lizdifan091105@users.noreply.github.com>
2024-04-03 18:39:35 +0700

    commitmoduljs

--- spread.js
+++ spread.js
@@ -13,3 +13,27 @@ tanda [ ]), karena memang mencetak nilai favorites itu sendiri. Dengan
 menggunakan spread operator dapat menyebarkan nilai-nilai dalam array tersebut.*/
 console.log(...favorites);
 // outputnya Seafood Salad Nugget soup
+/*pread operator bekerja seperti meleburkan nilai array menjadi beberapa elemen 
+sesuai panjang nilai array-nya. Sehingga jika menuliskan kode seperti ini: 
+console.log(...favorites); 
+Sama seperti kita menuliskan kode seperti ini: 
+console.log(favorites[0], favorites[1], favorites[2], 
+favorites[3]); 
+Spread operator dapat digunakan untuk menggabungkan dua buah array ke dalam 
+array baru. Jika tidak menggunakan spread operator ini maka hasilnya akan seperti*/
+const favorits=['Seafood','Salad','nugget','Soup']
+const others = ['Cake','pie','Donut']
+
+const allFavorits=[favorits, others];
+
+console.log(allFavorits);
+/*Nilai array tidak akan tergabung. Alih-alih menggabungkan nilainya, variabel 
+allFavorites menjadi array baru yang menampung dua array di dalamnya. Nah, 
+bagaimana jika menggunakan spread operator.*/
+const favorit=['Seafood','Salad','nugget','Soup']
+const other= ['Cake','pie','Donut']
+
+const allFavorit=[...favorit, ...other];
+
+console.log(allFavorit);
+//outputnya ' [Seafood', 'Salad', 'nugget', 'Soup', 'Cake', 'pie', 'Donut']
