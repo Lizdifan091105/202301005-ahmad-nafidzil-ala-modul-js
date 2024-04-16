594edf949d3f39bcd2f1aeb6bc9d993854c98b3e
ahmad nafidzil ala <162394204+Lizdifan091105@users.noreply.github.com>
2024-04-16 12:43:56 +0700

    commitmoduljs

--- loop&iteration.js
+++ loop&iteration.js
@@ -42,3 +42,49 @@ for (let index in arra){
     console.log(index);
     console.log(arra[index])
 }
+
+//6.Break statement
+/*ada perulangan break juga dapat digunakan untuk keluar dari suatu perulangan dan 
+terus mengeksekusi kode setelah keluar dari perulangan*/
+
+
+let q =0 ;
+for (q; q<10;q++ ){
+    if (q===7) break;
+    console.log('perulangan ke'+q)
+
+}
+/* 7. continue Statement
+Penggunaan continue dilakukan jika kita ingin menghentikan eksekusi suatu 
+statement dalam suatu iteration dan melanjutkan kembali perulangan dengan 
+iteration selanjutnya sampai selesai*/
+let nomor =0
+for(nomor =0;nomor<6;nomor++){
+    if (nomor===3)continue;
+    console.log('perulangan ke '+nomor)
+}
+//outputnya 1 2 4 5
+
+/*labeled statement
+Dengan label statement kita dapat membuat sebuah identifier sebagai sebuah 
+statement yang menjadi acuan saat menggunakan break atau continue statement */
+
+let str ='';
+let idx =0
+
+loop :
+for (idx;idx<5;idx++){
+    if  (idx===2){
+        continue loop;
+    
+    }
+    str =str+idx;
+    console.log(str)
+    
+}
+/*outputnya
+0
+01
+013
+0134
+*/
